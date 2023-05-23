bb7c898448f2a29b8acaaa9db9b182ac3f1ae7d9
Khaairi <100757455+Khaairi@users.noreply.github.com>
2023-05-23 20:00:23 +0700

    Add files via upload

---
+++ tp_mvc.sql
@@ -0,0 +1,144 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 23, 2023 at 02:59 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tp_mvc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `members` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`id`, `name`, `members`) VALUES
+(9, 'New Jeans', 1),
+(11, 'Test', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `join_date` varchar(255) NOT NULL,
+  `groups` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `members`
+--
+
+INSERT INTO `members` (`id`, `name`, `email`, `phone`, `join_date`, `groups`) VALUES
+(31, 'Hanni', 'hanni@mail.com', '08612891', '2023-05-23', 9);
+
+--
+-- Triggers `members`
+--
+DELIMITER $$
+CREATE TRIGGER `addMembers` AFTER INSERT ON `members` FOR EACH ROW BEGIN
+	UPDATE groups SET members = members + 1
+    WHERE id = NEW.groups;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `deleteMembers` BEFORE DELETE ON `members` FOR EACH ROW BEGIN
+	UPDATE groups SET members = members - 1
+    WHERE id = OLD.groups;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `updateMembers1` AFTER UPDATE ON `members` FOR EACH ROW BEGIN
+	UPDATE groups SET members = members + 1
+    WHERE id = NEW.groups;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `updateMembers2` BEFORE UPDATE ON `members` FOR EACH ROW BEGIN
+	UPDATE groups SET members = members - 1
+    WHERE id = OLD.groups;
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `groups` (`groups`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `members`
+--
+ALTER TABLE `members`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `members`
+--
+ALTER TABLE `members`
+  ADD CONSTRAINT `members_ibfk_1` FOREIGN KEY (`groups`) REFERENCES `groups` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
